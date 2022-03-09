64564e25796ebbb2d083220cbd2fda148b2ce650
Shreeya <shreeya@Shreeyas-MacBook-Pro.local>
2022-03-09 15:55:26 +0000

    added user menu

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Nav.scss";
+import UserMenu from "../UserMenu/UserMenu";
 
-const Nav= (handleClick) => {
+const Nav= ({handleClick}) => {
+  const [showUserMenu, setShowUserMenu] = useState(false);
+
+  const handleUserClick = () => {
+    setShowUserMenu(!showUserMenu);
+  };
+
   return (
     <div className="navbar">
       <img className="navbar-logo" src = "https://api.iconify.design/emojione-monotone/letter-n.svg?color=white" alt="logo"/>
+      
+      {showUserMenu && (
+        <UserMenu handleClick={handleUserClick}/>
+      )}
       <img 
         className = "navbar-userMenu" 
-        onClick={handleClick}
+        onClick={handleUserClick}
         src = "https://api.iconify.design/bxs/user-circle.svg?color=white"
         alt="user-login" 
       />
---
+++ src/components/UserMenu/UserMenu.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./UserMenu.scss"
+
+
+const UserMenu = ({handleClick, handleSubmit}) => {
+  return (
+    <div className="user-menu">
+      <img 
+        src= "https://api.iconify.design/akar-icons/cross.svg?color=white"  
+        className="user-menu__cross"
+        alt="Close menu"
+        onClick={handleClick}
+      />
+      <div className="user-menu__content">
+        <img src="https://api.iconify.design/bxs/user-circle.svg?color=white"className="user-menu__profile" alt="User profile" />
+        <h2 className="user-menu__title"> User Login</h2>
+        <form className="user-menu__form" onSubmit={handleSubmit}> 
+          <label for="first_name" className="user-menu__form--label"> First name: </label>
+          <input type="text" id="first_name" name="first_name" className="user-menu__form--input"/>
+          <label for="last_name" className="user-menu__form--label">Last name:</label>
+          <input type="text" id="last_name" name="last_name" className="user-menu__form--input"/>
+          <button className="user-menu__button" >Login</button>    
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default UserMenu; 
